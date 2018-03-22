30ee5b6d97167baf02ad1887d0e3b821cf5a2f69
st <{GH_USER_EMAIL}>
2018-03-22 20:28:51 +0100

    Fix version script

--- add-version.sh
+++ add-version.sh
@@ -9,7 +9,7 @@ fi
 VERSION_NAME=`echo $VERSION | sed 's/.Final//' | sed 's/.CR[[:digit:]]//'`
 NEWS_NAME=`date +%F`
 
-sed "s/VERSION/$VERSION/" src/web/version-template.json > src/web/versions/$VERSION_NAME.json
-sed "s/VERSION/$VERSION/" src/web/news-template.json > src/web/news/$NEWS_NAME.json
+sed "s/VERSION/$VERSION/" src/main/resources/version-template.json > src/main/resources/versions/$VERSION_NAME.json
+sed "s/VERSION/$VERSION/" src/main/resources/news-template.json > src/main/resources/news/$NEWS_NAME.json
 
 
