27047c48cf24f8a32d197555ec889c6beebec017
Luca Filipozzi <luca.filipozzi@gmail.com>
2021-08-24 06:31:35 -0700

    Create impersonation-policy-enforcers.java

---
+++ extensions/impersonation-policy-enforcers.java
@@ -0,0 +1,9 @@
+{
+    "name": "Impersonation Policy Enforcer",
+    "description": "This extension provides a Keycloak browser authenticator that enforces an impersonation policy restricting impersonators from accessing service providers (clients) unless holding an associated client role.",
+    "maintainers": [
+        "LucaFilipozzi"
+    ],
+    "website": "https://github.com/LucaFilipozzi/keycloak-impersonation-policy-enforcer",
+    "download": "https://github.com/LucaFilipozzi/keycloak-impersonation-policy-enforcer/releases"
+}
