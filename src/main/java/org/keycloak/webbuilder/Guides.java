36b203bde5768116b7a99204056670eba3d7614d
stianst <stianst@gmail.com>
2022-02-01 10:33:55 +0100

    Add support for pinning guides to the top
    Closes #267

--- src/main/java/org/keycloak/webbuilder/Guides.java
+++ src/main/java/org/keycloak/webbuilder/Guides.java
@@ -2,16 +2,17 @@ package org.keycloak.webbuilder;
 
 import org.keycloak.webbuilder.utils.AsciiDoctor;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Guides {
@@ -41,7 +42,7 @@ public class Guides {
 
         Collections.sort(guides, (o1, o2) -> {
             if (o1.getPriority() == o2.getPriority()) {
-                return o1.getName().compareTo(o2.getName());
+                return o1.getTitle().compareTo(o2.getTitle());
             } else {
                 return Integer.compare(o1.getPriority(), o2.getPriority());
             }
@@ -60,18 +61,45 @@ public class Guides {
     }
 
     private void loadGuides(AsciiDoctor asciiDoctor, File d, GuideCategory category) throws IOException {
+        Map<String, Integer> guidePriorities = loadPinnedGuides(new File(d, "pinned-guides"));
+
         for (File f: d.listFiles((dir, name) -> name.endsWith(".adoc") && !name.equals("index.adoc"))) {
             Map<String, Object> attributes = asciiDoctor.parseAttributes(f);
 
-            int priority = attributes.containsKey("guide-priority") ? Integer.parseInt((String) attributes.get("guide-priority")) : 1000;
-
             boolean community = "true".equals(attributes.get("community"));
-
             try {
-                Guide g = new Guide(category, f, (String) attributes.get("guide-title"), (String) attributes.get("guide-summary"), (String) attributes.get("guide-tags"), (String) attributes.get("author"), community, priority, (String) attributes.get("external-link"));
+                Guide g = new Guide(category, f, (String) attributes.get("guide-title"), (String) attributes.get("guide-summary"), (String) attributes.get("guide-tags"), (String) attributes.get("author"), community, (String) attributes.get("external-link"));
+
+                if (guidePriorities != null) {
+                    Integer priority = guidePriorities.get(g.getName());
+                    if (priority != null) {
+                        g.priority = priority;
+                    }
+                } else if (attributes.containsKey("guide-priority")) {
+                    g.priority = Integer.parseInt((String) attributes.get("guide-priority"));
+                }
+
                 guides.add(g);
             } catch (IllegalArgumentException e) {
             }
+        }
+    }
+
+    private Map<String, Integer> loadPinnedGuides(File pinnedGuides) throws IOException {
+        if (!pinnedGuides.isFile()) {
+            return null;
+        }
+        Map<String, Integer> priorities = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(pinnedGuides))) {
+            int c = 1;
+            for (String l = br.readLine(); l != null; l = br.readLine()) {
+                l = l.trim();
+                if (!l.isEmpty()) {
+                    priorities.put(l, c);
+                }
+                c++;
+            }
+            return priorities;
         }
     }
 
@@ -109,10 +137,10 @@ public class Guides {
         private String summary;
         private String path;
         private List<String> tags;
-        private int priority = -1;
+        private int priority = Integer.MAX_VALUE;
         private String externalLink;
 
-        public Guide(GuideCategory category, File source, String title, String summary, String tags, String author, boolean community, int priority, String externalLink) {
+        public Guide(GuideCategory category, File source, String title, String summary, String tags, String author, boolean community, String externalLink) {
             this.category = category;
             this.name = source.getName().replace(".adoc", "");
             this.author = author;
@@ -125,7 +153,6 @@ public class Guides {
                 this.tags = Arrays.stream(tags.split(",")).map(s -> s.toLowerCase().trim()).sorted().collect(Collectors.toList());
             }
             this.path = category.getId() + "/" + name;
-            this.priority = priority;
             this.externalLink = externalLink;
         }
 
