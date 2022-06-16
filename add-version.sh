26279fa3dfe97ff460a0633933b1066e95b422e6
Stian Thorgersen <stianst@gmail.com>
2022-06-16 11:51:00 +0200

    Update version for Keycloak guides (#322)
    Closes #321

--- add-version.sh
+++ add-version.sh
@@ -10,3 +10,10 @@ VERSION_NAME=`echo $VERSION | sed 's/.Final//' | sed 's/.CR[[:digit:]]//'`
 DATE=`date +%F`
 
 cat version-template.json | sed "s/VERSION/$VERSION/" | sed "s/DATE/$DATE/" > versions/$VERSION_NAME.json
+
+
+CURRENT=`cat pom.xml | grep '<version.keycloak>' | cut -d '>' -f 2 | cut -d '<' -f 1`
+LATEST=`echo -e "$CURRENT\n$VERSION" | sort -V -r | head -n 1`
+
+sed -i "s|<version.keycloak>$CURRENT</version.keycloak>|<version.keycloak>$LATEST</version.keycloak>|g" pom.xml
+
