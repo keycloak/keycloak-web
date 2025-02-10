abbe19defdbd2d6d0acad664307e47915e843ab7
Jon Koops <jonkoops@gmail.com>
2025-02-10 12:38:22 +0100

    Generalize `add-version.sh` script to allow versioning other repos (#563)
    Closes #562
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- add-version.sh
+++ add-version.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
 VERSION=$1
+ID=${2:-'keycloak'}
+
 if [ "$VERSION" == "" ]; then
-    echo "usage: add-version.sh VERSION"
+    echo "usage: add-version.sh VERSION [ID]"
     exit 1
 fi
 
@@ -12,7 +14,7 @@ DATE=`date +%F`
 if [[ "$VERSION" == *".0" ]]; then
     TEMPLATE="version-template.json"
 else
-    TEMPLATE="versions/keycloak/${VERSION%.*}.0.json"
+    TEMPLATE="versions/$ID/${VERSION%.*}.0.json"
 fi
 
 if [ ! -f "$TEMPLATE" ]; then
@@ -20,10 +22,10 @@ if [ ! -f "$TEMPLATE" ]; then
     exit
 fi
 
-cat $TEMPLATE | sed "s/\"version\":.*/\"version\": \"$VERSION\",/" | sed 's/"date": ".*"/"date": "DATE"/' | sed "s/DATE/$DATE/" > versions/keycloak/$VERSION_NAME.json
+cat $TEMPLATE | sed "s/\"version\":.*/\"version\": \"$VERSION\",/" | sed 's/"date": ".*"/"date": "DATE"/' | sed "s/DATE/$DATE/" > versions/$ID/$VERSION_NAME.json
 
-CURRENT=`cat pom.xml | grep '<version.keycloak>' | cut -d '>' -f 2 | cut -d '<' -f 1`
+CURRENT=`cat pom.xml | grep "<version.$ID>" | cut -d '>' -f 2 | cut -d '<' -f 1`
 LATEST=`echo -e "$CURRENT\n$VERSION" | sort -V -r | head -n 1`
 
-mvn versions:set-property -Dproperty=version.keycloak -DnewVersion=$LATEST -DgenerateBackupPoms=false
+mvn versions:set-property -Dproperty=version.$ID -DnewVersion=$LATEST -DgenerateBackupPoms=false
 mvn install
